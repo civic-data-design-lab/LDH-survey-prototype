c0998251c303acd914609ad6ce92907d3d04c5a5
Kwesi Afrifa <kwesiafrifa@gmail.com>
2021-03-02 17:19:16 -0500

    added sign-up auth; error alerts. TODO: incorporate sweetalerts

--- js/app.js
+++ js/app.js
@@ -25,16 +25,16 @@ function signIn(){
     // var checkUserPasswordValid = userPassword.match(userPasswordFormat);
 
     if(checkUserEmailValid == null){
+        alert("Invalid email format.")
         return checkUserEmail();
     // }else if(checkUserPasswordValid == null){
     //     return checkUserSIPassword();
     }else{
         firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).then((data) => {
-            const idToken = firebase.auth().currentUser.getIdToken()
+            const user = firebase.auth().currentUser
+            const idToken = user.getIdToken()
+            localStorage.setItem('email', user.email)
             return idToken
-        })
-        .then((token) => {
-            return 
         })
         .then((value) => {
             setTimeout(function(){
@@ -46,6 +46,57 @@ function signIn(){
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log(errorCode, errorMessage)
+            if (errorCode === "auth/user-not-found"){
+                alert("You do not have an account. Please sign-up to access KPSPIN.")
+            }
         });
     }
+}
+
+function signUp(){
+    var userEmail = document.getElementById("inputEmail").value;
+    var userPassword = document.getElementById("inputCreatePassword").value;
+    var userPasswordConfirm = document.getElementById("inputConfirmPassword").value
+    var userEmailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    var checkUserEmailValid = userEmail.match(userEmailFormat);
+
+    if (userPassword !== userPasswordConfirm) {
+        alert("Ensure both passwords match.")
+        return 
+    } 
+    else if (checkUserEmailValid == null){
+        alert("Email invalid")
+        return checkUserEmail();
+    }
+    else{
+        firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).then((data) => {
+            const user = firebase.auth().currentUser
+            const idToken = user.getIdToken()
+            const uid = user.uid
+            return idToken
+        })
+        .then((value) => {
+            setTimeout(function(){
+                window.location.replace("./index.html");
+            }, 1000)
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, errorMessage)
+            if (errorCode === "auth/email-already-in-use"){
+                alert("Already signed in.")
+                setTimeout(function(){
+                    window.location.replace("./index.html");
+                }, 1000)
+            }
+        })
+    }
+
+
+
+
+
+
 }
